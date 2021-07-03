f9c1f80e0c699f414823e5cb4b9dcb3ee2a11542
Glitch (hello-webpage) <none>
2021-07-03 22:25:35 +0000

    🙎🐟 Checkpoint ./js/function.js:22843327/3305 ./css/style.css:22843327/16 ./index.html:22843327/11

--- js/function.js
+++ js/function.js
@@ -276,7 +276,8 @@ function updateForm(blockData){
       .attr('id','section-'+i);
   
     
-    let svg=`<svg class="inline form-edit" width="24" height="22"><use href="#${sData.type}-icon"></svg>`
+    let svg=`<svg class="inline form-edit" width="24" height="22"><use href="#${sData.type}-icon"></svg>`;
+    
     let newSection=d3.select('#section-'+i);
     newSection.append('label')
       .text(sData.key+":")
@@ -304,10 +305,6 @@ function updateForm(blockData){
           generateTag(newString,d3.select(d3.event.target.parentNode).select('input').node())
         });
         
-          
-          
-          
-
       //<div class="tag" id="tag-lifestyle"><input type="checkbox" data-tag="lifestyle"><p>lifestyle</p></div>
       break;
       case "url":
@@ -348,6 +345,67 @@ function updateForm(blockData){
 }
 
 
+function generateField(type,value,key,index){
+  //add check for if index is defined
+  d3.select('#arena-goby')
+      .append('div')
+      .attr('class',"form-section type-"+type)
+      .attr('id','section-'+index);
+  let svg=`<svg class="inline form-edit" width="24" height="22"><use href="#${type}-icon"></svg>`;
+  let newSection=d3.select('#section-'+index);
+    newSection.append('label')
+      .text(key+":")
+      .node()
+      .insertAdjacentHTML('afterbegin',svg);
+  switch(type){
+      case "array":
+        sData.values.forEach((tag,t)=>{
+          newSection.append('div').attr('class','tag').node().dataset.tag=t;
+          let newTag=newSection.select(`[data-tag="${t}"]`);
+          newTag.append('input').attr('type','checkbox');
+          newTag.append('p').text(tag);
+          if(gobyBlock[sData.key].find(a=>a==tag)!==undefined){
+            newTag.select('input').property('checked',true);
+          }
+        })
+        newSection.append('div').attr('class','add-new-tag form-edit')
+        .append('input').attr('class','new-tag-input').attr('type','text').attr('data-fieldname',sData.key).attr('data-index',i).attr('tabindex',i+2);
+          
+        newSection.select('.add-new-tag')
+        .append('button').attr('class','plus-button').attr('type','button').html('+').on('click',function(){
+          let newString=d3.select(d3.event.target.parentNode).select('input').property('value');
+          generateTag(newString,d3.select(d3.event.target.parentNode).select('input').node())
+        });
+        
+      //<div class="tag" id="tag-lifestyle"><input type="checkbox" data-tag="lifestyle"><p>lifestyle</p></div>
+      break;
+      case "url":
+        newSection.select('label').classed('form-edit',true);
+        newSection.append('input').attr('class','form-edit').attr('type','text')
+          .property('value',gobyBlock[sData.key])
+          .attr('tabindex',i+2);
+        newSection.append('a').attr('target','_blank').attr('class','form-display').text(sData.key).attr('href',gobyBlock[sData.key])
+        .node()
+        .insertAdjacentHTML('afterbegin',svg);
+      break;
+      case "string":
+        newSection.append('input').attr('type','text').attr('class','form-edit')
+          .property('value',gobyBlock[sData.key])
+          .attr('tabindex',i+2);
+        newSection.append('p').attr('class','form-display short-text').text(gobyBlock[sData.key]);
+      break;
+      case "par":
+        newSection.append('textarea')
+          .attr('class','form-edit')
+          .html(gobyBlock[sData.key])
+          .attr('tabindex',i+2);
+        newSection.append('p').attr('class','form-display long-text').text(marked(gobyBlock[sData.key]));
+      break;
+        
+    }
+}
+
+
 function addNewSetUp(){
   d3.selectAll('button.choose-type').on('click',function(){
   d3.select('#add-new').attr('data-type',d3.event.currentTarget.dataset.type);
@@ -357,14 +415,20 @@ function addNewSetUp(){
     d3.select('#add-new').classed('panel-2',false);
   })
   
-  d3.select('#generate-field').on('click',function(){
-    generateField()
+  d3.select('#add-field').on('click',function(){
+    let newType=d3.select('#add-new').attr('data-type');
+    let newKey=d3.select('#add-new input').property('value')
+    addField(newType,newKey);
   })
 }
 
-function generateField(){
-  
+function addField(type,key){
+  if(!goby.manifest.find(a=>a.key==key)){
+    console.log('here comes a new one:',type,key);
+  }
 }
+
+
 
 
 
