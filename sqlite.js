7982087024ec0e9f2c3fd0c8786d356fee63f7d4
nchilla <jnico.chilla@gmail.com>
2022-03-23 20:50:23 -0400

    imported my typeface collection into goby's SQLite architecture for demo

--- sqlite.js
+++ sqlite.js
@@ -1,18 +1,14 @@
 import Database from 'better-sqlite3';
-// require Database from 'better-sqlite3';
-// require('better-sqlite3');
-
-//a function that initializes a new project, for the moment in memory
-
-
-
-
 
 
 class Project{
   constructor(source){
     // ':memory:'
     this.db= new Database(source);
+
+
+    //add conditional here to check if database is new or existing
+
     //System table to contain all objects in the project.
     this.createTable('system','root',['id INTEGER NOT NULL PRIMARY KEY']);
     // [TO ADD: special junction table for root objects to reference themselves in individual relation]
@@ -25,15 +21,30 @@ class Project{
     this.createTable('class','base');
 
     //a starting item (ID #1) in the object root table
-    // this.db.prepare('INSERT INTO system_root VALUES (null)').run();
+
     // this.db.prepare('INSERT INTO class_base VALUES (null)').run();
+    // console.log(this.db.prepare('INSERT INTO system_root VALUES (null)').run());
+  }
+
+  addEmptyRow(class_name,name_value){
+    //first add new row to root and get id
+    this.db.prepare('INSERT INTO system_root VALUES (null)').run();
+    const root_id=this.db.prepare('SELECT id FROM system_root ORDER BY id DESC').get().id;
+    //get the last item in class table and use it to get the order for the new item
+    const last_order=this.db.prepare(`SELECT system_order FROM [class_${class_name}] ORDER BY system_order DESC`).get();
+    const new_order=last_order?last_order.system_order+1:1;
+    //add new row with order and root id to class table
+    this.db.prepare(`INSERT INTO [class_${class_name}] (system_id,system_order,user_Name) VALUES (${root_id},${new_order},'${name_value}')`).run();
+    return root_id;
+
   }
 
   createTable(type,name,columns){
     //type will pass in 'class' or 'system' to use as a name prefix
     //columns will for now pass in an array of raw SQL column strings
 
-    columns=columns?columns:['id INTEGER UNIQUE','name TEXT',`FOREIGN KEY(id) REFERENCES system_root(id)`];
+    // columns=columns?columns:[`FOREIGN KEY(system_id) REFERENCES system_root(id)`,'system_id INTEGER UNIQUE','user_name TEXT'];
+    columns=columns?columns:['system_id INTEGER UNIQUE','system_order INTEGER','user_Name TEXT',`FOREIGN KEY(system_id) REFERENCES system_root(id)`];
 
     let column_string=columns.join(',');
     const sqlname=type=='class'?`[class_${name}]`:`${type}_${name}`;
@@ -47,7 +58,7 @@ class Project{
     const table_meta={
       properties:[
         {
-          name:'name',
+          name:'Name',
           type:'data',
           count:'single',
           type:'string',
@@ -67,10 +78,6 @@ class Project{
 
   //return 1. a SQL string and 2. a JSON with property data
   addProperty(class_name,class_id,prop_name,prop_type,options,count,label){
-    //prop_type is data or relation
-    //options is a JSON with type-specific instructions
-    //count is single or multiple
-    //label is true or false, makes it part of label if true
 
     //options examples---------------------
     const data_options_example={
@@ -106,7 +113,7 @@ class Project{
 
 
     //configure SQL command according to type and options
-
+    console.log('---------',prop_type);
     if(prop_type=='data'){
       //if it's data
       let sql_datatype='';
@@ -120,7 +127,7 @@ class Project{
         sql_datatype='REAL';
       }
       //create property in table
-      let command_string=`ALTER TABLE [class_${class_name}] ADD COLUMN [${prop_name}] ${sql_datatype}  ${options.default?('DEFAULT '+options.default):''};`;
+      let command_string=`ALTER TABLE [class_${class_name}] ADD COLUMN [user_${prop_name}] ${sql_datatype}  ${options.default?('DEFAULT '+options.default):''};`;
       this.db.prepare(command_string).run();
       //update metadata json for table with new property
       // this.db.prepare(`UPDATE system_classlist set metadata = '${JSON.stringify(class_meta)}' WHERE id = ${class_id}`).run();
@@ -142,9 +149,6 @@ class Project{
       //this will have been filled in on the front-end, or right after finishing a link declaration on the other side
       if(!prop_meta.junction_id){
         //IF no suitable existing junction table, create a new one:
-
-
-
         //sql column strings for create table
         const columns=[];
 
@@ -203,14 +207,15 @@ class Project{
         prop_meta.pointy_end=true;
       }
 
-
+      for (const target of prop_meta.targets){
+        delete target.prop_name;
+        delete target.count;
+      };
     }
 
     //update metadata json for table with new property
-    for (const target of prop_meta.targets){
-      delete target.prop_name;
-      delete target.count;
-    };
+
+
 
 
     this.db.prepare(`UPDATE system_classlist set metadata = '${JSON.stringify(class_meta)}' WHERE id = ${class_id}`).run();
@@ -218,70 +223,76 @@ class Project{
   }
 
 
+  addToJunction(junction_id,class_ids,row_ids){
+    this.db.prepare(`INSERT INTO junction_${junction_id} (${class_ids.join(',')}) VALUES (${row_ids.join(',')})`).run();
+  }
 
-  //not sure if prepared statements will be useful but leaving a note in case
-  preparedUniversals(){
-    // const stmts=[
-    //   ['get-tables','.tables'];
-    // ]
+  customImport(items,class_id,class_name){
+
+    let class_meta=JSON.parse(this.db.prepare(`SELECT metadata FROM system_classlist WHERE id = ${class_id}`).get().metadata);
+    //get junction id for foundry, classification, style
+    const junction_ids={
+      'Foundry':class_meta.properties.find(a=>a.name=='Foundry').junction_id,
+      'Classification':class_meta.properties.find(a=>a.name=='Classification').junction_id,
+      'Style':class_meta.properties.find(a=>a.name=='Style').junction_id
+    }
+    const class_ids={
+      'Foundry':class_meta.properties.find(a=>a.name=='Foundry').targets[0].class_id,
+      'Classification':class_meta.properties.find(a=>a.name=='Classification').targets[0].class_id,
+      'Style':class_meta.properties.find(a=>a.name=='Style').targets[0].class_id
+    }
+    const plurals={
+      'Foundry':'Foundries',
+      'Classification':'Classifications',
+      'Style':'Styles'
+    }
+
+
+    for(const item of items){
+      //first add new row to root and get id
+      this.db.prepare('INSERT INTO system_root VALUES (null)').run();
+      const root_id=this.db.prepare('SELECT id FROM system_root ORDER BY id DESC').get().id;
+      //get the last item in class table and use it to get the order for the new item
+      const last_order=this.db.prepare(`SELECT system_order FROM [class_${class_name}] ORDER BY system_order DESC`).get();
+      const new_order=last_order?last_order.system_order+1:1;
+
+      const columns=[];
+      const values=[];
+      // const relations=[];
+
+      //for each key:
+      for (let [key, value] of Object.entries(item)) {
+        if(key=='Name'||key=='Website'){
+          //adds name and website straight-up
+          columns.push(`[user_${key}]`);
+          values.push(`'${value}'`);
+        }else{
+          // relations.push({key:key,val:value});
+          const relations=value.split(',');
+
+          for(const relation of relations){
+            //if it's a relation (foundry, classification, style):
+            //looks for matching name in corresponding table
+            const relation_row=this.db.prepare(`SELECT system_id FROM class_${plurals[key]} WHERE user_Name LIKE '${relation}'`).get();
+
+            //if it can't find it, it creates a new row in that table and returns the ID
+            //if it can find it, it returns the ID
+            const relation_id=relation_row?relation_row.system_id:this.addEmptyRow(plurals[key],relation);
+            //with both IDs in hand, find the appropriate junction table for that property, and add row with IDs
+            this.addToJunction(junction_ids[key],['class_'+class_id,'class_'+class_ids[key]],[root_id,relation_id]);
+          }
+        }
+      }
+      //add main item to class table with data columns, order and root id
+      this.db.prepare(`INSERT INTO [class_${class_name}] (system_id,system_order,${columns.join(',')}) VALUES (${root_id},${new_order},${values.join(',')})`).run();
+
+
+    }
+
+
 
   }
 
-
 }
 
-const project=new Project(':memory:');
-
-
-// project.addProperty(
-//   'base',
-//   '1',
-//   'test-relation',
-//   'relation',
-//   {
-//     linked:false,
-//     targets:[
-//       {
-//         class_id:null,
-//         class_name:'test',
-//         prop_name:null,
-//         count:null
-//       }
-//     ],
-//     default:null
-//   },
-//   'single',
-//   false
-// );
-project.addProperty(
-  'base',
-  '1',
-  'test-relation',
-  'relation',
-  {
-    linked:true,
-    targets:[
-      {
-        class_id:null,
-        class_name:'test',
-        prop_name:'testing',
-        count:'single'
-      }
-    ],
-    default:null
-  },
-  'single',
-  false
-);
-//test output
-let print_tests=[
-  // project.db.prepare('SELECT * FROM system_classlist').all(),
-  'junction table: ------------',
-  JSON.parse(project.db.prepare('SELECT * FROM system_junctionlist').get().metadata),
-  'base class: ------------',
-  JSON.parse(project.db.prepare(`SELECT metadata FROM system_classlist WHERE name = 'base'`).get().metadata).properties,
-  'test class: ------------',
-  JSON.parse(project.db.prepare(`SELECT metadata FROM system_classlist WHERE name = 'test'`).get().metadata).properties,
-  // project.db.prepare('SELECT * FROM sqlite_schema WHERE rootpage=5').get()
-]
-console.log(print_tests);
+export default Project;
---
+++ testing-lib.js
@@ -0,0 +1,118 @@
+import Database from 'better-sqlite3';
+// var Project = require('./sqlite');
+import Project from './sqlite.js';
+
+
+// const fs = require('fs');
+import * as fs from 'fs';
+import * as util from 'util';
+// const util = require('util');
+
+
+
+
+
+
+
+const project=new Project('typefaces.db');
+
+
+project.createTable('class','Typefaces');
+
+project.addProperty(
+  'Typefaces',
+  '2',
+  'Foundry',
+  'relation',
+  {
+    linked:true,
+    targets:[
+      {
+        class_id:null,
+        class_name:'Foundries',
+        prop_name:'Typefaces',
+        count:'multiple'
+      }
+    ],
+    default:null
+  },
+  'single',
+  false
+);
+
+project.addProperty(
+  'Typefaces',
+  '2',
+  'Classification',
+  'relation',
+  {
+    linked:false,
+    targets:[
+      {
+        class_id:null,
+        class_name:'Classifications'
+      }
+    ],
+    default:null
+  },
+  'multiple',
+  false
+);
+
+project.addProperty(
+  'Typefaces',
+  '2',
+  'Style',
+  'relation',
+  {
+    linked:false,
+    targets:[
+      {
+        class_id:null,
+        class_name:'Styles'
+      }
+    ],
+    default:null
+  },
+  'multiple',
+  false
+);
+
+project.addProperty(
+  'Typefaces',
+  '2',
+  'Website',
+  'data',
+  {
+    type:'resource'
+  },
+  'single',
+  false
+)
+
+async function getTypefaces(){
+  let rawdata = await fs.readFileSync('typeface-data.json');
+  let parsed = JSON.parse(rawdata);
+  // console.log(parsed.values);
+
+  return parsed.values;
+}
+
+
+
+async function importTypefaces(){
+  const typefaces=await getTypefaces();
+  await project.customImport(typefaces,'2','Typefaces');
+
+  //test output
+  let print_tests=[
+    // JSON.parse(project.db.prepare(`SELECT metadata FROM system_classlist WHERE name = 'Typefaces'`).get().metadata).properties
+    project.db.prepare('SELECT * FROM [class_Typefaces]').all(),
+    project.db.prepare('SELECT * FROM junction_1').all()
+  ]
+  console.log(print_tests);
+
+  project.db.close();
+}
+
+importTypefaces();
